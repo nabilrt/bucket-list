0f58472aa6718b1d6a8bb663fbac8699c654937f
Abidur Rahman Nabil <nabilrt51@gmail.com>
2026-03-04 16:39:44 +0600

    add type annotation to image drawable

--- app/src/main/java/com/abidnabil/nabilbuckets/BucketListItem.java
+++ app/src/main/java/com/abidnabil/nabilbuckets/BucketListItem.java
@@ -1,12 +1,14 @@
 package com.abidnabil.nabilbuckets;
 
+import androidx.annotation.DrawableRes;
+
 public class BucketListItem {
     int itemId;
     String itemName;
     String itemDescription;
     int itemImage;
 
-    BucketListItem(int itemId, String itemName, String itemDescription, int itemImage) {
+    BucketListItem(int itemId, String itemName, String itemDescription, @DrawableRes int itemImage) {
         this.itemId = itemId;
         this.itemName = itemName;
         this.itemDescription = itemDescription;
